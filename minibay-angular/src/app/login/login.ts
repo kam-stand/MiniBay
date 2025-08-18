f0ad31f1ca11cc3808338deb4bc9b2dedde0befa
kamstand <kamrul.hassan@stonybrook.edu>
2025-08-17 20:39:40 -0400

    created login and signup page, need to create backend

--- minibay-angular/src/app/login/login.ts
+++ minibay-angular/src/app/login/login.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+interface login {
+  username:string;
+  password:string;
+}
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+
+  loginRequest: login = {
+    username: '',
+    password: ''
+  }
+
+  submitLogin(event:Event) {
+    event.preventDefault();
+    console.log('Form submitted')
+    console.log('Password: ' + this.loginRequest.password);
+    console.log('Username ' + this.loginRequest.username);
+  }
 
 }
--- minibay-angular/src/app/signup/signup.ts
+++ minibay-angular/src/app/signup/signup.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+interface signup {
+  username:string;
+  email:string;
+  password:string;
+  phonenumer: string;
+}
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css'
 })
 export class Signup {
+
+
+   signupRequest: signup = {
+    username: '',
+    password: '',
+    email: '',
+    phonenumer: ''
+  }
+
+  submitLogin(event:Event) {
+    event.preventDefault();
+    console.log('Form submitted')
+    console.log('Username ' + this.signupRequest.username);
+    console.log('Email: ' + this.signupRequest.email);
+    console.log('Phone number ' + this.signupRequest.phonenumer);
+    console.log('Password: ' + this.signupRequest.password);
+  }
 
 }
